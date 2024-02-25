0852aa05a099dbd3bc2398a6da1fbf1ea9350f9f
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-02-25 17:13:02 +0700

    try again

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import rootRoute from "./src/routes/rootRoutes.js";
+import ServerlessHttp from "serverless-http";
 
 const app = express();
 const port = 10000;
@@ -9,10 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use(rootRoute);
-// app.use("/.netlify/functions/api", rootRoute);
+app.use("/.netlify/functions/api", rootRoute);
 
 app.listen(port, () => {
   console.log(`⚡ Server is running at port:${port}`);
 });
 
-export const handler = serverless(app);
+export const handler = ServerlessHttp(app);
